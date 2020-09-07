63789e0e9543ec50c1ebb54bdac3c7234ea58e7a
Arsalan Yasir <arsalan@nullbrainer.io>
2020-09-07 12:18:33 +0500

    Updates

---
+++ precache-manifest.70f10f7ba2bac7a77a63477343e373ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78006eb281a949fe2dc6ca51f4c5fde9",
+    "url": "/git@github.com:arsalanyasir1/app.git/index.html"
+  },
+  {
+    "revision": "b90bc363cc97dbce63a8",
+    "url": "/git@github.com:arsalanyasir1/app.git/static/css/main.b47b3b74.chunk.css"
+  },
+  {
+    "revision": "92756991f722857ec6ca",
+    "url": "/git@github.com:arsalanyasir1/app.git/static/js/2.f3f70430.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/git@github.com:arsalanyasir1/app.git/static/js/2.f3f70430.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b90bc363cc97dbce63a8",
+    "url": "/git@github.com:arsalanyasir1/app.git/static/js/main.bb51bdf2.chunk.js"
+  },
+  {
+    "revision": "a44c88e8fdea20f9a019",
+    "url": "/git@github.com:arsalanyasir1/app.git/static/js/runtime-main.1240e9a8.js"
+  }
+]);
